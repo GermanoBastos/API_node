10edc8034e2b6b3e3d5eb6328cfe5e92717972d9
Germano Bastos <germano.silveira.neto@gmail.com>
2023-05-31 16:03:03 -0300

    f

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
-const express = require("express") // importado o express
-const app = express() // atribuindo o app ao express
-app.use(express.json())
-const rotaLivro = require("./rotas/livro")
+// const express = require("express") // importado o express
+// const app = express() // atribuindo o app ao express
+// app.use(express.json())
+// const rotaLivro = require("./rotas/livro")
 
-const port = 8000 // definindo a porta da aplicacao
+// const port = 8000 // definindo a porta da aplicacao
 
-// ESSA ROTA FOI TRANSFERIDA PARA PASTA ROTAS
-// rota na raiz com meto get exibindo no navegador amsg
-// app.get('/',(req,res)=>{
-//   res.send("Ola, esse é o método GET!")
+// // ESSA ROTA FOI TRANSFERIDA PARA PASTA ROTAS
+// // rota na raiz com meto get exibindo no navegador amsg
+// // app.get('/',(req,res)=>{
+// //   res.send("Ola, esse é o método GET!")
+// // })
+
+// // definindo rota com base num arquivo externo
+// app.use('/livros', rotaLivro)
+
+
+// // modo de escuta da porta especificada
+// app.listen(port,()=>{
 // })
 
-// definindo rota com base num arquivo externo
-app.use('/livros', rotaLivro)
+const express = require('express');
+const rotaLivro = require('./rotas/livro');
+const app = express();
+const port = process.env.PORT || 3000;
 
+app.use(express.json());
 
-// modo de escuta da porta especificada
-app.listen(port,()=>{
-})
+app.use('/livros', rotaLivro);
+
+app.get('/', (req, res) => {
+  res.send("Bem-vindo");
+});
+
+app.listen(port, () => {
+  console.log(`Escutando a porta ${port}`);
+});
