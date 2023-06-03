f6e013cd8edb5097014e5f5d52e91ce9d34bfbd8
Germano Bastos <germano.silveira.neto@gmail.com>
2023-06-03 17:19:04 -0300

    vercel ini

--- app.js
+++ app.js
@@ -1,24 +1,3 @@
-é: // const express = require("express") // importado o express
-// const app = express() // atribuindo o app ao express
-// app.use(express.json())
-// const rotaLivro = require("./rotas/livro")
-
-// const port = 8000 // definindo a porta da aplicacao
-
-// // ESSA ROTA FOI TRANSFERIDA PARA PASTA ROTAS
-// // rota na raiz com meto get exibindo no navegador amsg
-// // app.get('/',(req,res)=>{
-// //   res.send("Ola, esse é o método GET!")
-// // })
-
-// // definindo rota com base num arquivo externo
-// app.use('/livros', rotaLivro)
-
-
-// // modo de escuta da porta especificada
-// app.listen(port,()=>{
-// })
-
  express = require('express');
 const rotaLivro = require('./rotas/livro');
 const app = express();
