dcf9710d7b2fb7d5395c177941590a599ef8f226
Germano Bastos <germano.silveira.neto@gmail.com>
2023-06-04 12:57:12 -0300

    f

--- app.js
+++ app.js
@@ -8,8 +8,19 @@ app.use(express.json());
 app.use(cors());
 app.use("/livros", rotaLivro);
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
+
+app.get('/', (req, res) => {
+  try {
+    const dados = fs.readFileSync(path.join(__dirname, 'livros.json'));
+    const livros = JSON.parse(dados);
+    res.json(livros);
+  } catch (error) {
+    console.error('Erro ao listar os livros:', error);
+    res.status(500).send('Erro ao listar os livros');
+  }
 });
 
 app.listen(port, () => {
