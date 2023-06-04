925d548f040903f24add19c32bd12077af31aded
Germano Bastos <germano.silveira.neto@gmail.com>
2023-06-04 12:50:56 -0300

    ul

--- script.js
+++ script.js
@@ -1,157 +1,137 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // Inicialização do Materialize
-  M.AutoInit();
 
-  // Elementos do DOM
-  const livrosTableBody = document.getElementById('livrosTableBody');
-  const livroNomeInput = document.getElementById('nome');
-  const livroAutorInput = document.getElementById('autor');
-  const livroRevendaInput = document.getElementById('revenda');
-  const livroNomeEditarInput = document.getElementById('livroNomeEditar');
-  const livroIdEditarInput = document.getElementById('livroIdEditar');
-  const livroAutorEditarInput = document.getElementById('livroAutorEditar');
-  const livroRevendaEditarInput = document.getElementById('livroRevendaEditar');
+    document.addEventListener('DOMContentLoaded', function() {
+      const livroIdEditarInput = document.getElementById('livroIdEditar');
+      const livroNomeEditarInput = document.getElementById('livroNomeEditar');
+      const livroAutorEditarInput = document.getElementById('livroAutorEditar');
+      const livroRevendaEditarInput = document.getElementById('livroRevendaEditar');
 
-  // Função para exibir os livros na tabela
-  function exibirLivros(livros) {
-    livrosTableBody.innerHTML = '';
+      const livroNomeInput = document.getElementById('livroNome');
+      const livroAutorInput = document.getElementById('livroAutor');
+      const livroRevendaInput = document.getElementById('livroRevenda');
 
-    livros.forEach(livro => {
-      const row = document.createElement('tr');
-      row.innerHTML = `
-        <td>${livro.id}</td>
-        <td>${livro.nome}</td>
-        <td>${livro.autor}</td>
-        <td>${livro.revenda}</td>
-        <td>
-          <a href="#modalEditarLivro" class="editar-livro modal-trigger" data-id="${livro.id}" data-nome="${livro.nome}" data-autor="${livro.autor}" data-revenda="${livro.revenda}">
-            <i class="material-icons">edit</i>
-          </a>
-          <a href="#!" class="excluir-livro" data-id="${livro.id}">
-            <i class="material-icons">delete</i>
-          </a>
-        </td>
-      `;
-      livrosTableBody.appendChild(row);
-    });
+      const tabelaLivros = document.getElementById('tabelaLivros');
 
-    // Adicionar eventos aos botões de editar e excluir
-    const editarLivroButtons = document.getElementsByClassName('editar-livro');
-    const excluirLivroButtons = document.getElementsByClassName('excluir-livro');
+      // Função para carregar os livros na tabela
+      function carregarLivros() {
+        fetch('livros')
+          .then(response => response.json())
+          .then(data => {
+            let html = '';
+            data.forEach(livro => {
+              html += `
+                <tr>
+                  <td>${livro.id}</td>
+                  <td>${livro.nome}</td>
+                  <td>${livro.autor}</td>
+                  <td>${livro.revenda}</td>
+                  <td>
+                    <button class="btn editar-livro" data-id="${livro.id}" data-nome="${livro.nome}" data-autor="${livro.autor}" data-revenda="${livro.revenda}">Editar</button>
+                    <button class="btn red excluir-livro" data-id="${livro.id}">Excluir</button>
+                  </td>
+                </tr>
+              `;
+            });
+            tabelaLivros.innerHTML = html;
+          });
+      }
 
-    Array.from(editarLivroButtons).forEach(button => {
-      button.addEventListener('click', function() {
-        const id = button.getAttribute('data-id');
-        const nome = button.getAttribute('data-nome');
-        const autor = button.getAttribute('data-autor');
-        const revenda = button.getAttribute('data-revenda');
+      // Função para limpar os campos de adicionar livro
+      function limparCamposAdicionarLivro() {
+        livroNomeInput.value = '';
+        livroAutorInput.value = '';
+        livroRevendaInput.value = '';
+      }
 
+      // Função para preencher os campos do modal de editar livro
+      function preencherFormEditarLivro(id, nome, autor, revenda) {
         livroIdEditarInput.value = id;
         livroNomeEditarInput.value = nome;
         livroAutorEditarInput.value = autor;
         livroRevendaEditarInput.value = revenda;
+      }
+
+      // Função para adicionar um novo livro
+      document.getElementById('formAdicionarLivro').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const livroNome = livroNomeInput.value;
+        const livroAutor = livroAutorInput.value;
+        const livroRevenda = livroRevendaInput.value;
+
+        fetch('/livros', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              nome: livroNome,
+              autor: livroAutor,
+              revenda: livroRevenda
+            })
+          })
+          .then(() => {
+            carregarLivros();
+            limparCamposAdicionarLivro();
+          });
       });
+
+      // Função para editar um livro
+      document.getElementById('formEditarLivro').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const id = livroIdEditarInput.value;
+        const nome = livroNomeEditarInput.value;
+        const autor = livroAutorEditarInput.value;
+        const revenda = livroRevendaEditarInput.value;
+
+        fetch(`/livros/${id}`, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              nome: nome,
+              autor: autor,
+              revenda: revenda
+            })
+          })
+          .then(() => {
+            carregarLivros();
+            M.Modal.getInstance(document.getElementById('modalEditarLivro')).close();
+          });
+      });
+
+      // Evento de clique em editar livro
+      tabelaLivros.addEventListener('click', function(event) {
+        if (event.target.classList.contains('editar-livro')) {
+          const id = event.target.getAttribute('data-id');
+          const nome = event.target.getAttribute('data-nome');
+          const autor = event.target.getAttribute('data-autor');
+          const revenda = event.target.getAttribute('data-revenda');
+
+          preencherFormEditarLivro(id, nome, autor, revenda);
+          M.Modal.getInstance(document.getElementById('modalEditarLivro')).open();
+        }
+      });
+
+      // Evento de clique em excluir livro
+      tabelaLivros.addEventListener('click', function(event) {
+        if (event.target.classList.contains('excluir-livro')) {
+          const id = event.target.getAttribute('data-id');
+
+          fetch(`/livros/${id}`, {
+              method: 'DELETE'
+            })
+            .then(() => {
+              carregarLivros();
+            });
+        }
+      });
+
+      // Inicializar Materialize Modal
+      const modalEditarLivro = document.getElementById('modalEditarLivro');
+      M.Modal.init(modalEditarLivro);
+
+      // Carregar os livros iniciais
+      carregarLivros();
     });
-
-    Array.from(excluirLivroButtons).forEach(button => {
-      button.addEventListener('click', function() {
-        const id = button.getAttribute('data-id');
-        excluirLivro(id);
-      });
-    });
-  }
-
-  // Função para buscar todos os livros
-  function getLivros() {
-    fetch('/livros')
-      .then(response => response.json())
-      .then(data => {
-        exibirLivros(data);
-      })
-      .catch(error => {
-        console.error('Erro ao buscar os livros:', error);
-      });
-  }
-
-  // Função para adicionar um livro
-  function adicionarLivro() {
-    const nome = livroNomeInput.value;
-    const autor = livroAutorInput.value;
-    const revenda = livroRevendaInput.value;
-
-    fetch('/livros', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ nome, autor, revenda })
-    })
-      .then(response => {
-        if (response.ok) {
-          M.toast({ html: 'Livro adicionado com sucesso!' });
-          livroNomeInput.value = '';
-          livroAutorInput.value = '';
-          livroRevendaInput.value = '';
-          getLivros();
-        } else {
-          throw new Error('Erro ao adicionar livro');
-        }
-      })
-      .catch(error => {
-        console.error('Erro ao adicionar livro:', error);
-      });
-  }
-
-  // Função para atualizar um livro
-  function atualizarLivro() {
-    const id = livroIdEditarInput.value;
-    const nome = livroNomeEditarInput.value;
-    const autor = livroAutorEditarInput.value;
-    const revenda = livroRevendaEditarInput.value;
-
-    fetch(`/livros/${id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ nome, autor, revenda })
-    })
-      .then(response => {
-        if (response.ok) {
-          M.toast({ html: 'Livro atualizado com sucesso!' });
-          getLivros();
-        } else {
-          throw new Error('Erro ao atualizar livro');
-        }
-      })
-      .catch(error => {
-        console.error('Erro ao atualizar livro:', error);
-      });
-  }
-
-  // Função para excluir um livro
-  function excluirLivro(id) {
-    fetch(`/livros/${id}`, {
-      method: 'DELETE'
-    })
-      .then(response => {
-        if (response.ok) {
-          M.toast({ html: 'Livro excluído com sucesso!' });
-          getLivros();
-        } else {
-          throw new Error('Erro ao excluir livro');
-        }
-      })
-      .catch(error => {
-        console.error('Erro ao excluir livro:', error);
-      });
-  }
-
-  // Event listeners
-  document.getElementById('btnSalvarLivro').addEventListener('click', adicionarLivro);
-  document.getElementById('btnAtualizarLivro').addEventListener('click', atualizarLivro);
-
-  // Carregar livros ao carregar a página
-  getLivros();
-});
-
