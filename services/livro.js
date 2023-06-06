65583893e12a27c1c235ea9c5c7239f0c5be48b7
Germano Bastos <germano.silveira.neto@gmail.com>
2023-06-05 22:01:01 -0300

    df

--- services/livro.js
+++ services/livro.js
@@ -1,69 +1,61 @@
-const fetch = require("node-fetch");
+const fs = require("fs");
+const path = require("path");
 
-async function getTodosLivros() {
-  const response = await fetch("https://lucky-cassata-576e99.netlify.app/livros.json");
-  const data = await response.json();
-  return data;
+function getTodosLivros() {
+  const filePath = path.join(__dirname, "../livros.json");
+  const dados = fs.readFileSync(filePath);
+  return JSON.parse(dados);
 }
 
-async function getLivroPorId(id) {
-  const response = await fetch("https://lucky-cassata-576e99.netlify.app/livros.json");
-  const data = await response.json();
-  const livroFiltrado = data.find((livro) => livro.id == id);
+function getLivroPorId(id) {
+  const filePath = path.join(__dirname, "../livros.json");
+  const livros = JSON.parse(fs.readFileSync(filePath));
+  const livroFiltrado = livros.find((livro) => livro.id == id);
   return livroFiltrado;
 }
 
-async function getNextLivroId() {
-  const response = await fetch("https://lucky-cassata-576e99.netlify.app/livros.json");
-  const data = await response.json();
-  const lastLivro = data[data.length - 1];
+function getNextLivroId() {
+  const filePath = path.join(__dirname, "../livros.json");
+  const livros = JSON.parse(fs.readFileSync(filePath));
+  const lastLivro = livros[livros.length - 1];
   return lastLivro ? lastLivro.id + 1 : 1;
 }
 
-async function insereLivro(livroNovo) {
-  const response = await fetch("https://lucky-cassata-576e99.netlify.app/livros.json");
-  const data = await response.json();
+function insereLivro(livroNovo) {
+  const filePath = path.join(__dirname, "../livros.json");
+  const livros = JSON.parse(fs.readFileSync(filePath));
   const novoLivro = {
-    id: await getNextLivroId(),
+    id: getNextLivroId(),
     nome: livroNovo.nome,
     autor: livroNovo.autor,
     revenda: livroNovo.revenda,
   };
-  const novaListaDeLivros = [...data, novoLivro];
+  const novaListaDeLivros = [...livros, novoLivro];
 
-  await fetch("https://lucky-cassata-576e99.netlify.app/livros.json", {
-    method: "PUT",
-    body: JSON.stringify(novaListaDeLivros),
-  });
+  fs.writeFileSync(filePath, JSON.stringify(novaListaDeLivros));
 }
 
-async function atualizaLivro(id, livroAtualizado) {
-  const response = await fetch("https://lucky-cassata-576e99.netlify.app/livros.json");
-  const data = await response.json();
-  const livroIndex = data.findIndex((livro) => livro.id == id);
+function atualizaLivro(id, livroAtualizado) {
+  const filePath = path.join(__dirname, "../livros.json");
+  const livros = JSON.parse(fs.readFileSync(filePath));
+  const livroIndex = livros.findIndex((livro) => livro.id == id);
 
   if (livroIndex !== -1) {
-    data[livroIndex] = { ...data[livroIndex], ...livroAtualizado };
-    await fetch("https://lucky-cassata-576e99.netlify.app/livros.json", {
-      method: "PUT",
-      body: JSON.stringify(data),
-    });
+    livros[livroIndex] = { ...livros[livroIndex], ...livroAtualizado };
+    fs.writeFileSync(filePath, JSON.stringify(livros));
   } else {
     throw new Error("Livro não encontrado");
   }
 }
 
-async function removeLivro(id) {
-  const response = await fetch("https://lucky-cassata-576e99.netlify.app/livros.json");
-  const data = await response.json();
-  const livroIndex = data.findIndex((livro) => livro.id == id);
+function removeLivro(id) {
+  const filePath = path.join(__dirname, "../livros.json");
+  const livros = JSON.parse(fs.readFileSync(filePath));
+  const livroIndex = livros.findIndex((livro) => livro.id == id);
 
   if (livroIndex !== -1) {
-    data.splice(livroIndex, 1);
-    await fetch("https://lucky-cassata-576e99.netlify.app/livros.json", {
-      method: "PUT",
-      body: JSON.stringify(data),
-    });
+    livros.splice(livroIndex, 1);
+    fs.writeFileSync(filePath, JSON.stringify(livros));
   } else {
     throw new Error("Livro não encontrado");
   }
